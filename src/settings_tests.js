a179d45ede91876ae0d1a2ec50b574e87d333590
ogourment <ogourment@smarterportal.com>
2015-02-07 00:42:05 +0100

    Better tests

--- src/settings_tests.js
+++ src/settings_tests.js
@@ -34,25 +34,85 @@ Tinytest.add("Simple defaults, null Meteor.settings", function (test) {
 
   Meteor.settings = null;
 
-  var simpleDefaults = { public: 1 };
+  var simpleDefaults = { serverValue: 1 };
 
   MeteorSettings.setDefaults( simpleDefaults );
 
-  test.equal( Meteor.settings, simpleDefaults );
+  var expected = { serverValue: 1, public: {} };
+
+  test.equal( Meteor.settings, expected );
 });
 
 Tinytest.add("Simple defaults, empty Meteor.settings", function (test) {
 
   Meteor.settings = {};
 
-  var simpleDefaults = { public: 1 };
+  var simpleDefaults = { serverValue: 1 };
+
+  MeteorSettings.setDefaults( simpleDefaults );
+
+  var expected = { serverValue: 1, public: {} };
+
+  test.equal( Meteor.settings, expected );
+});
+
+Tinytest.add("Simple public, null Meteor.settings", function (test) {
+
+  Meteor.settings = null;
+
+  var simpleDefaults = { public: { value: 1 } };
 
   MeteorSettings.setDefaults( simpleDefaults );
 
   test.equal( Meteor.settings, simpleDefaults );
 });
 
-Tinytest.add("Defaults to merge", function (test) {
+Tinytest.add("Simple public, empty Meteor.settings", function (test) {
+
+  Meteor.settings = {};
+
+  var simpleDefaults = { public: { value: 1 } };
+
+  MeteorSettings.setDefaults( simpleDefaults );
+
+  test.equal( Meteor.settings, simpleDefaults );
+});
+
+Tinytest.add("Merge public with public", function (test) {
+
+  Meteor.settings = { public: { value: 10 } };
+
+  var defaults = {
+    public: { value: 2, other: 3 }
+  };
+
+  MeteorSettings.setDefaults( defaults );
+
+  var expected = {
+    public: { value: 10, other: 3 }
+  }
+
+  test.equal( Meteor.settings, expected );
+});
+
+Tinytest.add("Merge public with different public", function (test) {
+
+  Meteor.settings = { public: { value: 2 } };
+
+  var defaults = {
+    public: { other: 3 }
+  };
+
+  MeteorSettings.setDefaults( defaults );
+
+  var expected = {
+    public: { value: 2, other: 3 }
+  }
+
+  test.equal( Meteor.settings, expected );
+});
+
+Tinytest.add("Merge public with public and server", function (test) {
 
   Meteor.settings = { public: { value: 10 } };
 
@@ -66,6 +126,39 @@ Tinytest.add("Defaults to merge", function (test) {
   var expected = {
     serverValue: 1,
     public: { value: 10, other: 3 }
+  }
+
+  test.equal( Meteor.settings, expected );
+});
+
+Tinytest.add("Merge empty settings with public", function (test) {
+
+  Meteor.settings = { };
+
+  var defaults = {
+    public: { value: 2, other: 3 }
+  };
+
+  MeteorSettings.setDefaults( defaults );
+
+  var expected = defaults;
+
+  test.equal( Meteor.settings, expected );
+});
+
+Tinytest.add("Merge server with public", function (test) {
+
+  Meteor.settings = { serverValue: 1 };
+
+  var defaults = {
+    public: { value: 2, other: 3 }
+  };
+
+  MeteorSettings.setDefaults( defaults );
+
+  var expected = {
+    serverValue: 1,
+    public: { value: 2, other: 3 }
   }
 
   test.equal( Meteor.settings, expected );
